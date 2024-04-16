ec41a231b6fc9119cecdb543825d5bb3ad2b6902
kfm351 <kfm351@yandex.ru>
2024-04-17 01:47:28 +0300

    1

--- Agents/Data/Data.cs
+++ Agents/Data/Data.cs
@@ -3,5 +3,6 @@
     public static class Data
     {
         public static List<Agent> AgentList = new List<Agent>();
+        public static List<(Agent, int)> AgentsJournal = new List<(Agent, int)>();
     }
 }
